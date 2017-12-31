d498754bee86b61fe111d2f26cde5d6e6fa3b89f
TengLee888 <coffeealcohol@gmail.com>
2017-12-31 15:45:47 +0800

    開結合書上範例，成功顯示註冊頁面

--- app.js
+++ app.js
@@ -1,59 +1,83 @@
 require('dotenv').config();
 var express = require('express');
-var app = express()
-var reg = require('./routes/reg.js')
-var flash = require('connect-flash')
-var path    = require("path");
-var favicon = require('serve-favicon')
-// app.use('/reg', reg);
-// app.use(flash());
-//
-//
-// var express     = require('express');
-// var app         = express();
-// var bodyParser  = require('body-parser');
-// var config = require('./config'); // get our config file
-// var router = require('./router');
-var port = process.env.PORT || 8012;
+var path    = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser  = require('body-parser');
+var flash = require('connect-flash');
+
+
+var routes = require('./routes/index');
+// var Users = require('./routes/users');
+
+var app = express();
+var port = process.env.PORT || 3000;
+app.set('views' , path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/view', express.static(__dirname + '/view'));
+app.use('/', routes);
 
 
-// routes
-// app.use(express.static('public'));
+//get home page
 app.get('/',function(req,res){
      res.sendFile(__dirname + '/index.html');
 });
 
 
+
+
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next){
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+//
+// //errow handler
+// //development error handler
+// //will print stacktrace
+// if(app.get('env') === 'development'){
+//   app.use(function(err , req , res , next){
+//     res.status(err.status || 500);
+//     res.render('error' , {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
+//
+//
+// //production error handler
+// //no stacktrace leaked to user
+// app.use(function(err , req , res , next){
+//   res.status(err.status || 500);
+//   res.render('error' , {
+//     message: err.message,
+//     error: err
+//   });
+// })
+
+
+
+
+
+
+
+
+
+
 // start the server
 app.listen(port);
-console.log('Magic happens at http://localhost:' + port);
-
-
-
-
-
-// var cool = require('cool-ascii-faces');
-// var express = require('express');
-// var app = express();
-//
-// app.set('port', (process.env.PORT || 5000));
-//
-// app.use(express.static(__dirname + '/public'));
-//
-// // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-//
-// app.get('/', function(request, response) {
-//   response.render('pages/index')
-// });
-//
-// app.get('/cool', function(request, response) {
-//   response.send(cool());
-// });
-//
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
+console.log('Server Start, http://localhost:' + port);
---
+++ routes/index.js
@@ -0,0 +1,48 @@
+// var express = require('express');
+// var router = express.Router()
+var express = require('express');
+var router = express.Router();
+// routes
+//get home page
+// router.get('/',function(req,res){
+//      res.sendFile('../index.html');
+// });
+
+//
+// // 用戶註冊
+// router.get('/reg' , function(req , res){
+//
+// });
+// router.post('/reg' , function(req , res){
+//
+// });
+//
+//
+// // 用戶登陸
+// router.get('/login' , function(req , res){
+//
+// });
+// router.post('/login' , function(req , res){
+//
+// });
+//
+//
+// //發表筆記
+// router.get('/post' , function(req , res){
+//
+// });
+//
+// router.post('/post' , function(req , res){
+//
+// });
+//
+//
+// // 登出
+// router.get('/' , function(req , res){
+//
+// });
+//
+
+
+// module.exports = router
+module.exports = router;
