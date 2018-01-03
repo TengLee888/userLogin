8e4d33d1471e698d8f37348c667f7a7e68824035
TengLee888 <coffeealcohol@gmail.com>
2018-01-03 17:01:20 +0800

    判斷用戶是否已註冊信箱，redirect頁面尚未成功

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,8 @@ var crypto = require('crypto');
 
 
 // Database
-var dbName ='heroku_xj85pq2p'
-var url ='mongodb://tenglee:Harvest888!!!@ds113282.mlab.com:13282/heroku_xj85pq2p'
-// var url = process.env.DB_URL;
-// var dbName = process.env.DB_NAME
+var url = process.env.DB_URL;
+var dbName = process.env.DB_NAME
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
@@ -28,6 +26,10 @@ router.get('/' , function(req , res){
 
 
 
+
+
+
+
 // 用戶註冊
 router.get('/reg' , function(req , res){
   res.render('reg')
@@ -38,7 +40,9 @@ router.post('/reg' , function(req , res){
   var password = req.body.password;
   var passwordRepeat = req.body.passwordRepeat
   if(password !== passwordRepeat){
-    req.flash('error' , '兩次輸入的密碼不一致')
+    // req.flash('error' , '兩次輸入的密碼不一致')
+    // res.send('兩次輸入的密碼不一致')
+    console.log('兩次輸入的密碼不一致');
     return res.redirect('/reg')
   }
 
@@ -56,64 +60,83 @@ router.post('/reg' , function(req , res){
   MongoClient.connect(url, function(err,client) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
-    const db = client.db(dbName);
+    var db = client.db(dbName);
 
-    insertDocuments(db, function() {
-      findDocuments(db, function() {
-        client.close();
-      });
+    //檢查用戶名是否已存在
+    db.collection('users').findOne({email:email}, function(err, result) {
+      if (err) throw err;
+      console.log("result: " , result);
+      if(!result){
+        console.log(result, "此信箱尚未註冊");
+        insertDocuments(db , function(){
+          db.close();
+        })
+        // res.redirect('/');
+      }
+
+      console.log(result, "此信箱已註冊");
+      db.close();
+      // res.redirect('/login');
     });
-  });
 
 
+  // var findDocuments = function(db, callback) {
+  //   var collection = db.collection('users');
+  //   collection.find({email: '123465236'}).toArray(function(err, docs) {
+  //     assert.equal(err, null);
+  //     console.log("Found the following records");
+  //     console.log(docs);
+  //     if(docs == []){
+  //       console.log(" 此信箱尚未註冊");
+  //       // insertDocuments()
+  //     }else{
+  //       console.log(" 此信箱已註冊");
+  //     }
+  //     callback(docs);
+  //   });
+    // console.log(result)
+    // if(result){
+    //   console.log(" 此信箱已註冊");
+    //   // req.flash('error' , '此信箱已註冊');
+    //   // res.send('此信箱已註冊');  //傳回login page
+    // }else{
+    //   console.log("開始註冊");
+    //   // insertDocuments(db, function() {
+    //   // });
+    // }
+    // callback(result);
+  })
 
+
+  // 如果不存在則新增用戶
   // 輸入資料  Insert a Document
-  const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('users');
-    // Insert some documents
+  var insertDocuments = function(db, callback) {
+    var collection = db.collection('users');
     collection.insertOne(
-    newUser ,
-    function(err, result) {
-      console.log("Inserted user information into the collection");
-      callback(result);
-    });
-  }
-
-
-
-
-  // 搜尋資料 Find All Documents
-  const findDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('users');
-    // Find some documents
-    collection.find({}).toArray(function(err, docs) {
-      assert.equal(err, null);
-      console.log("Found the following records");
-      console.log(docs)
-      callback(docs);
-    });
-  }
-
-  // //檢查用戶名是否已存在
-  // User.get(newUser.email , function(err, user){
-  //   if(user){
-  //     req.flash('error' , '此信箱已註冊');
-  //     return res.redirect('/login');  //傳回login page
-  //   }
-  // })
-  // // 如果不存在則新增用戶
-  // newUser.save(function(err, user){
-  //   if(err){
-  //     req.flash('error' , err);
-  //     return res.redirect('/reg'); //reg page
-  //   }
-  //   req.session.user = user; //使用者存入ssession
-  //   req.flash('sucess' , '註冊成功!');
-  //   res.redirect('/'); //註冊成功傳回主頁
-  // })
+      newUser,
+      function(err, result) {
+        console.log("Inserted documents into the collection: " , newUser);
+        res.redirect('/')
+        callback(result);
+      }
+    );
+  };
 });
+
+
+
+
+//   // 如果不存在則新增用戶
+//   newUser.save(function(err, user){
+//     if(err){
+//       req.flash('error' , err);
+//       return res.redirect('/reg'); //reg page
+//     }
+//     req.session.user = user; //使用者存入ssession
+//     req.flash('sucess' , '註冊成功!');
+//     res.redirect('/'); //註冊成功傳回主頁
+//   })
+// });
 
 
 
@@ -122,8 +145,9 @@ router.post('/reg' , function(req , res){
 
 // 用戶登陸
 router.get('/login',function(req,res){
-  res.render('login')
+  res.render('login');
 });
+// return res.redirect('/'); //回到主頁
 // router.post('/login' , function(req , res){
 //
 // });
@@ -144,6 +168,5 @@ router.get('/login',function(req,res){
 //
 // });
 //
-
 
 module.exports = router;
