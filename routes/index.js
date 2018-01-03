49631c463ab9cffa7a26a73a544fad8b2151446b
TengLee888 <coffeealcohol@gmail.com>
2018-01-03 17:02:34 +0800

    online testing

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,10 @@ var crypto = require('crypto');
 
 
 // Database
-var url = process.env.DB_URL;
-var dbName = process.env.DB_NAME
+var dbName ='heroku_xj85pq2p'
+var url ='mongodb://tenglee:Harvest888!!!@ds113282.mlab.com:13282/heroku_xj85pq2p'
+// var url = process.env.DB_URL;
+// var dbName = process.env.DB_NAME
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
